1e75da27c218ba6c06d09e22f8cb63237c088220
Ali Waqar <aliwaqar981@gmail.com>
2025-07-17 11:21:13 +0500

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="h1-bold">Tailwind CSS is fun</h1>
+      <h1 className="h1-bold font-inter">Tailwind CSS is fun [interfont]</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Tailwind CSS is fun [spaceGrotesk]
+      </h1>
     </div>
   );
 };
