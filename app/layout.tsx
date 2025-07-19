6d840bc9a90029a898c28662f0bfa147bffa5351
Ali Waqar <aliwaqar981@gmail.com>
2025-07-19 17:40:18 +0500

    setup theme and integrated shadCN library

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import LocalFonts from "next/font/local";
 import "./globals.css";
+import ThemeProvider from "@/context/Theme";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -34,11 +35,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
