ca7e235ab9f57706d265b58fc2c18e031320e0da
Ali Waqar <aliwaqar981@gmail.com>
2025-08-17 17:36:37 +0500

    Implement the UI of question form

--- lib/validations.ts
+++ lib/validations.ts
@@ -45,3 +45,26 @@ export const SignUpSchema = z.object({
       "Password must contain atleast one special character"
     ),
 });
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { error: "Title is required" })
+    .max(100, { error: "Title cannot exceed 100 characters" }),
+
+  content: z.string().min(1, { error: "Body is required" }),
+  tags: z
+    .array(z.string())
+    .min(1, {
+      error: "Atleast one tag is required",
+    })
+    .max(30, {
+      error: "Tags cannot exceed 30 characters",
+    })
+    .min(1, {
+      error: "Atleast one tag is required",
+    })
+    .max(3, {
+      error: "Cannot add more than 3 tags",
+    }),
+});
